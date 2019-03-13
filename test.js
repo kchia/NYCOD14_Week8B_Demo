fb14e21c5310356b584164508b83d50b9a7b07f1
EC2 Default User <ec2-user@ip-172-31-59-33.ec2.internal>
2019-03-13 01:52:21 +0000

    make a test fail on purpose

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ it('should return true', function(){
 }); 
 
 it('correctly calculates the sum of 1 and 3', () => {
-  assert.equal(operations.add(1, 3), 4);
+  assert.equal(operations.add(1, 3), 5);
 });
 
 it('correctly calculates the sum of -1 and -1', () => {
