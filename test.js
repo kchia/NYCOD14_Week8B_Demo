b3ad3f12b301f7239a8787484b33be66579788fb
EC2 Default User <ec2-user@ip-172-31-59-33.ec2.internal>
2019-03-26 00:20:09 +0000

    fix bug

--- test.js
+++ test.js
@@ -1,23 +1,19 @@
 var assert = require('assert');
 var operations = require('./index');
 
-it('should return true', function(){
-  assert.equal(true, true);
-}); 
-
-it('correctly calculates the sum of 1 and 3', () => {
-  assert.equal(operations.add(1, 3), 5);
+it('correctly calculates the sum of 1 and 3', function() {
+  assert.equal(operations.add(1, 3), 4);
 });
 
-it('correctly calculates the sum of -1 and -1', () => {
+it('correctly calculates the sum of -1 and -1', function() {
   assert.equal(operations.add(-1, -1), -2);
 });
 
-it('correctly calculates the difference of 33 and 3', () => {
+it('correctly calculates the difference of 33 and 3', function() {
   assert.equal(operations.subtract(33, 3), 30);
 });
 
-it('correctly calculates the product of 12 and 12', () => {
+it('correctly calculates the product of 12 and 12', function() {
   assert.equal(operations.multiply(12, 12), 144);
 });
 
